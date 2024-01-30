7a4e025df48459f2e0bdf491aaa4fb96b20b69de
margueellohenriquez <maarguello@arsys.es>
2024-01-30 10:49:48 +0100

    (lesson 27 course StencilJS)
    -Listening to Events Inside the Component

--- tooltip.js
+++ tooltip.js
@@ -1,12 +1,24 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
+    this._tooltipContainer;  
   }
 
   connectedCallback() {
       const tooltipText = document.createElement('span');
       tooltipText.textContent = ' (?)';
+      tooltipText.addEventListener('mouseenter', this._showTooltip.bind(this));
+      tooltipText.addEventListener('mouseleave', this._hideTooltip.bind(this));
       this.appendChild( tooltipText );
+  }
+
+  _showTooltip() {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = 'This is the tooltip text!';
+    this.appendChild( this._tooltipContainer );
+  }
+  _hideTooltip() {
+    this.removeChild( this._tooltipContainer );
   }
 }
 
