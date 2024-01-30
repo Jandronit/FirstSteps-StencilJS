ea73da6af08e291297cf26685e338c127aa39e6b
margueellohenriquez <maarguello@arsys.es>
2024-01-30 11:00:25 +0100

    (lesson 28 course StencilJS)
    -Using Attributes on Custom Elements

--- tooltip.js
+++ tooltip.js
@@ -1,22 +1,27 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
-    this._tooltipContainer;  
+    this._tooltipContainer;
+    this._tooltipText = 'Some dummy tooltip text.';
   }
 
   connectedCallback() {
-      const tooltipText = document.createElement('span');
-      tooltipText.textContent = ' (?)';
-      tooltipText.addEventListener('mouseenter', this._showTooltip.bind(this));
-      tooltipText.addEventListener('mouseleave', this._hideTooltip.bind(this));
-      this.appendChild( tooltipText );
+    if (this.hasAttribute('text')){
+      this._tooltipText = this.getAttribute('text');  
+    } 
+    const tooltipText = document.createElement('span');
+    tooltipText.textContent = ' (?)';
+    tooltipText.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipText.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    this.appendChild( tooltipText );
   }
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
-    this._tooltipContainer.textContent = 'This is the tooltip text!';
+    this._tooltipContainer.textContent = this._tooltipText;
     this.appendChild( this._tooltipContainer );
   }
+  
   _hideTooltip() {
     this.removeChild( this._tooltipContainer );
   }
