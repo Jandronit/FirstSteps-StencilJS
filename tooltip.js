1f7ca7d95ccd049c7d58170ffaf4734f30e699d5
margueellohenriquez <maarguello@arsys.es>
2024-01-30 11:37:10 +0100

    (lesson 30 course StencilJS)
    -Styling our Elements

--- tooltip.js
+++ tooltip.js
@@ -14,14 +14,19 @@ class Tooltip extends HTMLElement {
     tooltipText.addEventListener('mouseenter', this._showTooltip.bind(this));
     tooltipText.addEventListener('mouseleave', this._hideTooltip.bind(this));
     this.appendChild( tooltipText );
+    this.style.position = 'relative';
   }
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
     this._tooltipContainer.textContent = this._tooltipText;
+    this._tooltipContainer.style.backgroundColor = 'black';
+    this._tooltipContainer.style.color = 'white';
+    this._tooltipContainer.style.position = 'absolute';
+    this._tooltipContainer.style.zIndex = '10';
     this.appendChild( this._tooltipContainer );
   }
-  
+
   _hideTooltip() {
     this.removeChild( this._tooltipContainer );
   }
