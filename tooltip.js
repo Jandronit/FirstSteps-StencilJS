414d8167f19072f738ee78ca33168d094a69e0c4
margueellohenriquez <maarguello@arsys.es>
2024-02-01 10:30:34 +0100

    (lesson 49 course StencilJS)
    -Cleaning Up Our Overall Styling

--- tooltip.js
+++ tooltip.js
@@ -11,11 +11,17 @@ class Tooltip extends HTMLElement {
         background-color: black;
         color: white;
         position: absolute;
+        top: 1.5rem;
+        left: 0.75rem;
         z-index: 10;
+        padding: 0.15rem;
+        border-radius: 3px;
+        box-shadow: 1px 1px 6px rgba(0,0,0,0.26);
       }
 
       :host(.important) {
         background: var(--color-primary, #ccc);
+        padding: 0.15rem;
       }
 
       :host-context(p) {
