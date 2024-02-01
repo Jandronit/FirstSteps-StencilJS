1744a64c575f7b4e4aa8a89ed83e447a3f0aa104
margueellohenriquez <maarguello@arsys.es>
2024-02-01 14:15:01 +0100

    (lesson 58 course StencilJS)
    -Adding the Modal Container

--- complex-components/modal.js
+++ complex-components/modal.js
@@ -13,6 +13,17 @@ class Modal extends HTMLElement {
                 background: rgba(0, 0, 0, 0.75);
                 z-index: 10;
             }
+            #modal {
+                position: fixed;
+                top: 15vh;
+                left: 25%;
+                width: 50%;
+                height: 30rem;
+                background: white;
+                border-radius: 3px;
+                box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+                z-index: 100;
+            }
         </style>    
         <div id="backdrop"></div>
         <div id="modal">
