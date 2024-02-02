6286c2a15f3fd51c1a1875469bf06828c0193970
margueellohenriquez <maarguello@arsys.es>
2024-02-02 10:52:28 +0100

    (lesson 59 course StencilJS)
    -Stiling the Modal Elements

--- complex-components/modal.js
+++ complex-components/modal.js
@@ -18,22 +18,46 @@ class Modal extends HTMLElement {
                 top: 15vh;
                 left: 25%;
                 width: 50%;
-                height: 30rem;
                 background: white;
                 border-radius: 3px;
                 box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
                 z-index: 100;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+            }
+            header {
+                padding: 1rem;
+                border-bottom: 1px solid #ccc;
+            }
+            header h1 {
+                font-size: 1.25rem;
+            }
+
+            #main {
+                padding: 1rem;
+            }
+            
+            #actions {
+                border-top: 1px solid #ccc;
+                padding: 1rem;
+                display: flex;
+                justify-content: flex-end;
+            
+            }
+            #actions button {
+                margin: 0 0.25rem;
             }
         </style>    
         <div id="backdrop"></div>
         <div id="modal">
             <header>
-                <slot name="title"></slot>
+                <h1>Please confirm</h1>
             </header>
-            <section>
+            <section id="main">
                 <slot></slot>
             </section>
-            <footer>
+            <footer id="actions">
                 <button id="cancel-btn">Cancel</button>
                 <button id="confirm-btn">Confirm</button>
             </footer>
