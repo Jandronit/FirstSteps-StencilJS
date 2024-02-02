f575ba584e978d69540d7780d481a4cc363e4288
margueellohenriquez <maarguello@arsys.es>
2024-02-02 17:11:26 +0100

    (lesson 83-84 course StencilJS)
    -Styling a Stencil Component

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,13 +1,15 @@
 import { Component, h } from '@stencil/core';
 @Component({
-  tag: 'my-side-drawer'
+  tag: 'my-side-drawer',
+  styleUrl: './side-drawer.css',
+  shadow: true
 })
 export class SideDrawer {
   render() {
     return (
-      <div>
-        <h1>Hello World!!! Jandrohen</h1>
-      </div>
+      <aside>
+        <h1>The Side Drawer</h1>
+      </aside>
     );
   }
 }
