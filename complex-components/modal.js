ab87f013cf0af7f1ab29f88e60817d7d77edb3b1
margueellohenriquez <maarguello@arsys.es>
2024-02-02 12:00:29 +0100

    (lesson 61 course StencilJS)
    -Opening the Modal via CSS

--- complex-components/modal.js
+++ complex-components/modal.js
@@ -15,6 +15,13 @@ class Modal extends HTMLElement {
                 opacity: 0;
                 pointer-events: none;
             }
+
+            :host([opened]) #backdrop,
+            :host([opened]) #modal
+            {
+                opacity: 1;
+                pointer-events: all;
+            }
             #modal {
                 position: fixed;
                 top: 15vh;
@@ -69,6 +76,21 @@ class Modal extends HTMLElement {
         </div>
         `;
     }
+
+    // attributeChangedCallback(name, oldValue, newValue) {
+    //     if (name === 'opened') {
+    //         if (this.hasAttribute('opened')) {
+    //             this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
+    //             this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
+    //             this.shadowRoot.querySelector('#modal').style.opacity = 1;
+    //             this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
+    //         }
+    //     }
+    // }
+
+    // static get observedAttributes() {
+    //     return ['opened'];
+    // }
 }
 
 customElements.define('my-modal', Modal);
