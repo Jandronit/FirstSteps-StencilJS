98b3d2723d2a24e59d2446949cdaa4853bfad5e4
margueellohenriquez <maarguello@arsys.es>
2024-02-04 10:39:02 +0100

    (lesson 90 course StencilJS)
    -Using Attributes for Styling only

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -3,26 +3,36 @@ import { Component, Prop, h } from '@stencil/core';
   tag: 'my-side-drawer',
   styleUrl: './side-drawer.css',
   shadow: true
+
 })
 export class SideDrawer {
   @Prop({ reflect: true }) title: string;
-  @Prop() open: boolean;
+  // @Prop() open: boolean;
 
   render() {
-    let content = null;
-    if (this.open) {
-      content = (
-        <aside>
-          <header>
-            <h1>{this.title}</h1>
-          </header>
-          <main>
-            <slot />
-          </main>
-        </aside>
-      );
-    }
-    return content
+    // let content = null;
+    // if (this.open) {
+    //   content = (
+    //     <aside>
+    //       <header>
+    //         <h1>{this.title}</h1>
+    //       </header>
+    //       <main>
+    //         <slot />
+    //       </main>
+    //     </aside>
+    //   );
+    // }
+    return (
+    <aside>
+    <header>
+      <h1>{this.title}</h1>
+    </header>
+    <main>
+      <slot />
+    </main>
+  </aside>
+    );
   }
 
 }
